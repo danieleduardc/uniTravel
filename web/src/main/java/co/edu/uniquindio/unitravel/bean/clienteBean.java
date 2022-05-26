36422a3d458939c067788880827ff90b0bdceb73
danieleduardc <daniele.puertah@uqvirtual.edu.co>
2022-05-26 17:15:41 -0500

    version de pruebas formularios

---
+++ web/src/main/java/co/edu/uniquindio/unitravel/bean/clienteBean.java
@@ -0,0 +1,47 @@
+package co.edu.uniquindio.unitravel.bean;
+
+import co.edu.uniquindio.unitravel.entidades.Cliente;
+import co.edu.uniquindio.unitravel.servicios.ClienteServicio;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+
+@Component
+@ViewScoped
+public class clienteBean implements Serializable {
+
+    @Autowired
+    private ClienteServicio clienteServicio;
+
+    @Getter @Setter
+    private Cliente cliente;
+
+    @PostConstruct
+    public void init(){
+        cliente = new Cliente();
+    }
+    public void registraCliente(){
+        try {
+            clienteServicio.registrarCliente(cliente);
+
+            FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alerta",
+                    "Registro exitoso");
+            FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+
+        } catch (Exception e) {
+            FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta",
+                    e.getMessage());
+            FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+            e.printStackTrace();
+        }
+    }
+
+
+}
